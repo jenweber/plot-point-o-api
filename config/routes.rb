963f375dff9dc5ff82118c84105f4bfc9033f743
JenWeber <weberj10@gmail.com>
2016-02-26 14:20:00 -0500

    remove update post delete restrictions on posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
-  resources :users, only: [:index, :show]
-  resources :games, only: [:index, :show]
-  resources :spoilery_posts, only: [:index, :show]
-  resources :no_spoilers_posts, only: [:index, :show]
+  resources :users
+  resources :games
+  resources :spoilery_posts
+  resources :no_spoilers_posts 
 end
+
+# resources :games, only: [:index, :show]
+# resources :spoilery_posts, only: [:index, :show]
+# resources :no_spoilers_posts, only: [:index, :show]
