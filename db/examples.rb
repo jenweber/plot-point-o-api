d347cfbf300be87e649948a35178063da0eab340
JenWeber <weberj10@gmail.com>
2016-03-01 17:48:02 -0500

    attempt create admin

--- db/examples.rb
+++ db/examples.rb
@@ -1,4 +1,4 @@
-admin = User.create([{ email:'admin3@plotpointo.com' }, { password:'admin3' }, { password_confirmation:'admin3' }])
+admin = User.create([{ email:'admin4@plotpointo.com' }, { password:'admin3' }, { password_confirmation:'admin3' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # This file should contain all the record creation needed to experiment with
